00a7184dad0b69cf81db5a0a73871594c6ce7e69
Alya4565 <144199797+Alya4565@users.noreply.github.com>
2023-10-11 12:47:40 +0500

    Add files via upload

--- index.php
+++ index.php
@@ -2,22 +2,23 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
-        <Style>
-        .error{
-            color: red;
-        }
-        </style>
+        <title>Форма регистрации</title>
     </head>
     <body>
-        <form action="1.php" method="post">
-        <p>Почта:<span class="error">*</span> <input type="email" name="mail" placeholder= "E-Mail" required></p> 
-            <p>Логин:<span class="error">*</span> <input type="text" name="name"  placeholder= "Логин" required></p>
-            <p>Пароль:<span class="error">*</span> <input type="password" name="password"  placeholder= "Пароль" required></p>
-            <p>Подтверждение пароля:<span class="error">*</span> <input type="password" name="password1"  placeholder= "Потверждение пароля"  required></p>
-            <p><span class="error"> * обязательное поле </span></p>
-            <p><input type="submit" value="Зарегестрироваться" required></p> 
-           
+        <form action="actions.php" method="post">
+          <label for="mail">Почта
+             <input type="email" name="email" placeholder="E-mail" required></label><br><br>
+          
+           <label for="login">Логин
+             <input type="text" name="login" placeholder="login" required></label><br><br>
+          
+          <label for="password">Пароль
+             <input type="password" name="password" placeholder="Пароль" required ></label><br><br>
+          
+                <label for="password">Подтверждение пароля
+             <input type="password" name="password2" placeholder="Подтверждение пароля" required ></label><br><br>
+          
+            <p><input type ="submit" value="Зарегестрироваться" required</p> 
         </form>
     </body>
 </html>
