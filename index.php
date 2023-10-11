36f2390ac4a5a5a6bd343a605ec540db8e4cc455
Alya4565 <144199797+Alya4565@users.noreply.github.com>
2023-10-11 14:28:05 +0500

    Add files via upload

--- index.php
+++ index.php
@@ -1,24 +1,17 @@
-<html lang="ru">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Форма регистрации</title>
-    </head>
-    <body>
-        <form action="actions.php" method="post">
-          <label for="mail">Почта
-             <input type="email" name="email" placeholder="E-mail" required></label><br><br>
-          
-           <label for="login">Логин
-             <input type="text" name="login" placeholder="login" required></label><br><br>
-          
-          <label for="password">Пароль
-             <input type="password" name="password" placeholder="Пароль" required ></label><br><br>
-          
-                <label for="password">Подтверждение пароля
-             <input type="password" name="password2" placeholder="Подтверждение пароля" required ></label><br><br>
-          
-            <p><input type ="submit" value="Зарегестрироваться" required</p> 
-        </form>
-    </body>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>pr6</title>
+</head>
+<body>
+    <p><b>Заполните форму ниже</b></p>
+    <form action="action.php" method="post">
+        <p>Ваше имя: <input type="text" name="name" required></p>
+        <p>Ваш возраст: <input type="number" name="age" required></p>
+        <p><input type="submit"></p> 
+    </form>
+</body>
 </html>
